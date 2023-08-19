595b3d550d56440ba856d674f43e180bc8a2908b
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-19 13:34:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { fetchImages, PER_PAGE, API_KEY, BASE_URL } from './js/api';
 import refs from './js/refs';
+
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
+let pageNumber = 1;
+let currentHits = 0;
+let searchQuery = '';
+
+loadMoreBtn.style.display = 'none';
+endText.style.display = 'none';
+
+function renderImageList(images) {
+    console.log(images, 'images');
+    const markup = images
+      .map(image => {
+        console.log('img', image);
+        return `<div class="photo-card">
+         <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+          <div class="info">
+             <p class="info-item">
+      <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+  </p>
+              <p class="info-item">
+                  <b>Views</b> <span class="info-item-api">${image.views}</span>
+              </p>
+              <p class="info-item">
+                  <b>Comments</b> <span class="info-item-api">${image.comments}</span>
+              </p>
+              <p class="info-item">
+                  <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
+              </p>
+          </div>
+      </div>`;
+      })
+      .join('');
+    gallery.innerHTML += markup;
+  }
+
+searchForm.addEventListener('submit', onSubmitSearchForm);
