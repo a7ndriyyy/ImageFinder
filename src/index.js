ace6e598f5313eb3a2509b67d8eb7609f28d079e
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-19 13:20:37 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { fetchImages, PER_PAGE, API_KEY, BASE_URL } from './js/api';
+import refs from './js/refs';
---
+++ src/js/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = `https://pixabay.com/api/`;
+const API_KEY = '38613444-415ea31856756f0ffc19b62a0';
+const PER_PAGE = 40;
+
+async function fetchImages(page = 1, value) {
+  axios.defaults.params = {
+    key: API_KEY,
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: PER_PAGE,
+    page: page,
+  };
+  const response = await axios.get(BASE_URL);
+  return response.data;
+}
+
+export { fetchImages, PER_PAGE, BASE_URL, API_KEY };
