1710db53c13cdb01a26e10cbc09e92182d567973
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-19 13:31:07 +0200

    correct(

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+ searchForm: document.querySelector('.search-form'),
+ gallery: document.querySelector('.gallery'),
+ loadMoreBtn: document.querySelector('.load-more'),
+ endText: document.querySelector('.end-text'),
+  };
